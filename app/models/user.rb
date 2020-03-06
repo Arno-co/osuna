878be28fec26b0dd121ae37215942a316399db13
ArnoCo <acognard@gmail.com>
2020-03-06 14:44:02 -0800

    Remove presence true

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :email,  presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :session_token,  presence: true, uniqueness: true
     validates :password_digest,  presence: true
-    validates :password, length: {minimum: 6, allow_nil: true}, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
 
     attr_reader :password
     after_initialize :ensure_session_token
