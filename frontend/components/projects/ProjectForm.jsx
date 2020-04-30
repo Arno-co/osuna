7a53888ef4f45945053a98adbbacae262b6fb73e
ArnoCo <acognard@gmail.com>
2020-04-30 13:43:00 -0700

    Fix CSS buttons and cursors

--- frontend/components/projects/ProjectForm.jsx
+++ frontend/components/projects/ProjectForm.jsx
@@ -52,7 +52,7 @@ class ProjectForm extends React.Component {
                             </span>
                         </div>
                         <label className='project-label'>Title</label>
-                        <input className='project-field-title' type="text" value={this.state.title} onChange={this.update('title')} required/>
+                        <textarea className='project-field-title' value={this.state.title} onChange={this.update('title')} required/>
                         <label className='project-label'>Description</label>
                         <textarea className='project-field-description'  value={this.state.description} onChange={this.update('description')} form='project' rows="5" cols="33" ></textarea>
                         <div className='dates'>
@@ -61,7 +61,7 @@ class ProjectForm extends React.Component {
                             <label className='project-label'>Ends</label>
                             <input className='project-field' type="date" value={this.state.endDate} onChange={this.update('endDate')} required/>
                         </div>
-                        <input className='nav-button' type='submit' value={this.props.formType === 'CREATE A PROJECT' ? 'CREATE' : 'EDIT'} />
+                        <input className='project-form-nav-button' type='submit' value={this.props.formType === 'CREATE A PROJECT' ? 'CREATE' : 'EDIT'} />
                         {this.renderErrors()}
                     </form>
                 </div>
